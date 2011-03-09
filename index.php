572d78b6a98e149e5ac0c9b898356196a6cd5d95
Mike Helmick <mike.helmick@gmail.com>
2011-03-08 21:21:03 -0800

    add logos

--- index.php
+++ index.php
@@ -10,6 +10,15 @@
 <p>Our three-sided conference theme, "Teaching, Learning, and Collaborating," commemorates North Carolina's renowned "Research Triangle" where SIGCSE 2012 will be held. Teaching, learning, and collaborating occur inside and outside of the classroom, among various combinations of students, academics, industry professionals, and others.</p>
 <h2>Call for participation</h2>
 <p><a href="/sigcse2012/authors/index.php">View the SIGCSE 2012 call for participation.</a></p>
+<h2>Help select the SIGCSE 2012 Logo!</h2>
+<p>Stop by the SIGCSE 2012 both in the SIGCSE 2011 exhibit hall during the symposium (March 9 - 13th) in Dallas, TX. The winning logo will be announced on Saturday. The candidate logs appear below:</p>
+
+<table width="100%">
+<tr><td align="center"><img src="/sigcse2012/logo/logo01.png" /></td></tr>
+<tr><td align="center"><img src="/sigcse2012/logo/logo02.png" /></td></tr>
+<tr><td align="center"><img src="/sigcse2012/logo/logo03.png" /></td></tr>
+</td></tr></table>
+
 <?php
  $updated = filemtime( $_SERVER["SCRIPT_FILENAME"] );
  $mainPage = true;
