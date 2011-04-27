40253a01d5a4e16734679c6242ae153f36195531
Mike Helmick <mike.helmick@gmail.com>
2011-04-26 21:27:46 -0700

    Committee pictures. Closes #10.

--- committee/index.php
+++ committee/index.php
@@ -10,145 +10,162 @@
   <tr class="header"><td colspan="3" align="center">
 	<a href="index.php">PROGRAM COMMITTEE</a> | <a href="apc.php">ASSOCIATE PROGRAM CHAIRS</a></td></tr> 
   <tr><td align="left" valign="top">
-<p><strong>Symposium Chairs</strong><br /> 
+<p class="listing"><strong>Symposium Chairs</strong><br /> 
 Laurie Smith King<br /> 
 College of the Holy Cross<br />
+<img src="/sigcse2012/images/photos/laurie_king.jpg" />
 &nbsp;<br/>
 Dave Musicant<br/>
 Carleton College<br/>
-<script>write_email("sigcse12-symposium", "cs.holycross.edu");</script>
+<script>write_email("sigcse12-symposium", "cs.holycross.edu");</script><br/>
+<img src="/sigcse2012/images/photos/dave_musicant.jpg" />
 </p>
 
-<p>
+<p class="listing">
 <strong>Program Chairs</strong><br>
 Tracy Camp<br/>
 Colorado School of Mines<br/>
 &nbsp;<br/>
 Paul Tymann<br/>
 Rochester Institute of Technology<br/>
-<script>write_email("sigcse12-program", "cs.holycross.edu");</script></p>
+<script>write_email("sigcse12-program", "cs.holycross.edu");</script><br/>
+<img src="/sigcse2012/images/photos/tracy_camp.jpg" /></p>
 
-<p><b>Panels and Special Sessions</b><br/>
+<p class="listing"><b>Panels and Special Sessions</b><br/>
 Chuck Leska<br/>
 Randolph Macon College<br/>
 <script>write_email("sigcse12-panels", "cs.holycross.edu");</script><br/>
-<script>write_email("sigcse12-specials", "cs.holycross.edu");</script></p>
+<script>write_email("sigcse12-specials", "cs.holycross.edu");</script><br />
+<img src="/sigcse2012/images/photos/chuck_leska.jpg" /></p>
 
-<p><b>Workshops</b><br/>
+<p class="listing"><b>Workshops</b><br/>
 Adrienne Decker<br/>
 University at Buffalo (SUNY)<br/>
+<img src="/sigcse2012/images/photos/adrienne_decker.jpg" /><br />
 &nbsp;<br/>
 Lester I. McCann<br/>
 The University of Arizona<br/>
-<script>write_email("sigcse12-workshops", "cs.holycross.edu");</script>
-</p>
+<script>write_email("sigcse12-workshops", "cs.holycross.edu");</script><br />
+<img src="/sigcse2012/images/photos/lester_mccann.jpg" /></p>
 
-<p><b>Publications</b><br/>
+<p class="listing"><b>Publications</b><br/>
 Brad Miller<br/>
 Luther College<br/>
-<script>write_email("sigcse12-publications", "cs.holycross.edu");</script>
-</p>
+<script>write_email("sigcse12-publications", "cs.holycross.edu");</script><br />
+<img src="/sigcse2012/images/photos/brad_miller.jpg" /></p>
 
-<p><b>Registration</b><br/>
+<p class="listing"><b>Registration</b><br/>
 Cary Laxer and Lynn Degler<br/>
 Rose-Hulman Institute of Technology<br/>
+<img src="/sigcse2012/images/photos/cary_laxer.jpg" /> <br />
+<img src="/sigcse2012/images/photos/lynn_degler.png" /><br />
 &nbsp;<br/>
 Larry Merkle<br/>
 Wright State University<br/>
-<script>write_email( "sigcse12-registration", "cs.holycross.edu" );</script></p>
+<script>write_email( "sigcse12-registration", "cs.holycross.edu" );</script><br />
+<img src="/sigcse2012/images/photos/larry_merkle.jpg" />
+</p>
 
 </td><td align="left" valign="top">
 
-<p><b>Posters</b><br/>
+<p class="listing"><b>Posters</b><br/>
 Kris Nagel<br/>
 Georgia Gwinnett College<br/>
-<script>write_email("sigcse12-posters", "cs.holycross.edu");</script></p>
+<script>write_email("sigcse12-posters", "cs.holycross.edu");</script><br />
+<img src="/sigcse2012/images/photos/kristine_nagel.jpg" /></p>
 
-<p><b>Birds Of A Feather</b><br/>
+<p class="listing"><b>Birds Of A Feather</b><br/>
 Olaf Hall-Holt<br/>
 St. Olaf College<br/>
-<script>write_email("sigcse12-bofs", "cs.holycross.edu");</script>
-</p>
+<script>write_email("sigcse12-bofs", "cs.holycross.edu");</script><br />
+<img src="/sigcse2012/images/photos/olaf_hallholt.jpg" /></p>
 
-<p><b>Student Volunteers and Student Activities</b><br/>
+<p class="listing"><b>Student Volunteers and Student Activities</b><br/>
 Mary Anne Egan<br/>
 Siena College<br/>
+<img src="/sigcse2012/images/photos/maryanne_egan.jpg" /><br />
 &nbsp;<br/>
 Steven Huss-Lederman<br/>
 Beloit College<br/>
-<script>write_email("sigcse12-volunteers", "cs.holycross.edu");</script>
-</p>
+<script>write_email("sigcse12-volunteers", "cs.holycross.edu");</script><br />
+<img src="/sigcse2012/images/photos/steven_husslederman.jpg" /></p>
 
-<p><b>Treasurer</b><br/>
+<p class="listing"><b>Treasurer</b><br/>
 Scott McElfresh<br/>
 Wake Forest University<br/>
-<script>write_email("sigcse12-treasurer", "cs.holycross.edu");</script>
-</p>
+<script>write_email("sigcse12-treasurer", "cs.holycross.edu");</script><br />
+<img src="/sigcse2012/images/photos/scott_mcelfresh.jpg" /></p>
 
-<p><b>Database Administrators</b><br/>
+<p class="listing"><b>Database Administrators</b><br/>
 Henry Walker <br/>
 Grinnell College <br/>
-<br/> 
+<img src="/sigcse2012/images/photos/henry_walker.jpg" /><br />
+&nbsp;<br/> 
 John Dooley <br/>
 Knox College <br/>
-<script>write_email("sigcse12-database", "cs.holycross.edu");</script>
-</p>
+<script>write_email("sigcse12-database", "cs.holycross.edu");</script><br />
+<img src="/sigcse2012/images/photos/john_dooley.jpg" /></p>
 
-<p><b>Webmaster</b><br/>
+<p class="listing"><b>Webmaster</b><br/>
 Michael T. Helmick <br/>
 Google <br/>
-<script>write_email( "sigcse12-webmaster", "cs.holycross.edu" );</script></p>
+<script>write_email( "sigcse12-webmaster", "cs.holycross.edu" );</script><br/>
+<img src="/sigcse2012/images/photos/mike_helmick.jpg" /></p>
 
 </td><td align="left" valign="top">
 
-<p><b>Evaluations</b><br/>
+<p class="listing"><b>Evaluations</b><br/>
 Carl Alphonce<br/>
 University at Buffalo, SUNY<br/>
-<script>write_email("sigcse12-evaluations", "cs.holycross.edu");</script>
-</p>
+<script>write_email("sigcse12-evaluations", "cs.holycross.edu");</script><br />
+<img src="/sigcse2012/images/photos/carl_alphonce.jpg" /></p>
 
-<p><b>Kids' Camp</b><br/>
+<p class="listing"><b>Kids' Camp</b><br/>
 Susan Fox<br/>
 Macalester Collage<br/>
+<img src="/sigcse2012/images/photos/susan_fox.jpg" /><br />
 &nbsp;<br/>
 Sarah Monisha Pulimood<br/>
 The College of New Jersey<br/>
-<script>write_email("sigcse12-kidscamp", "cs.holycross.edu");</script>
-</p>
+<script>write_email("sigcse12-kidscamp", "cs.holycross.edu");</script><br />
+<img src="/sigcse2012/images/photos/sarahmonisha_pulimood.jpg" /></p>
 
-<p><b>Support/Exhibitor Liaison</b><br/>
+<p class="listing"><b>Support/Exhibitor Liaison</b><br/>
 Susan Rodger<br/>
 Duke University<br />
-<script>write_email("sigcse12-exhibitors", "cs.holycross.edu");</script></p>
+<script>write_email("sigcse12-exhibitors", "cs.holycross.edu");</script><br />
+<img src="/sigcse2012/images/photos/susan_rodger.jpg" /></p>
 
-<p><b>Pre-Conference Events Liaison</b><br/>
+<p class="listing"><b>Pre-Conference Events Liaison</b><br/>
 Briana Morrison<br/>
 Southern Polytechnic State University<br/>
-<script>write_email("sigcse12-preconference", "cs.holycross.edu");</script></p>
+<script>write_email("sigcse12-preconference", "cs.holycross.edu");</script><br />
+<img src="/sigcse2012/images/photos/briana_morrison.jpg" /></p>
 
-<p><b>K-12 Liaison</b><br/>
+<p class="listing"><b>K-12 Liaison</b><br/>
 John Harrison<br/>
 Princess Ann High School<br/>
 Virginia Beach, VA<br/>
-<script>write_email("sigcse12-k12", "cs.holycross.edu");</script>
+<script>write_email("sigcse12-k12", "cs.holycross.edu");</script><br />
 </p>
 
-<p><b>International Liaison</b><br/>
+<p class="listing"><b>International Liaison</b><br/>
 Raymond Lister<br/>
 University of Technology<br/>
-<script>write_email("sigcse12-international", "cs.holycross.edu");</script>
-</p>
+<script>write_email("sigcse12-international", "cs.holycross.edu");</script><br />
+<img src="/sigcse2012/images/photos/raymond_lister.jpg" /></p>
 
-<p><b>Local Arrangements</b><br/>
+<p class="listing"><b>Local Arrangements</b><br/>
 Sarah Heckman<br/>
 NC State University<br/>
-<script>write_email("sigcse12-local", "cs.holycross.edu");</script>
-</p>
+<script>write_email("sigcse12-local", "cs.holycross.edu");</script><br />
+<img src="/sigcse2012/images/photos/sarah_heckman.gif" /></p>
 
-<p><b>Student Research Competition</b><br/>
+<p class="listing"><b>Student Research Competition</b><br/>
 Ann Sobel <br/>
 Miami University (Ohio) <br/>
-<script>write_email( "sigcse12-src", "cs.holycross.edu" );</script></p>
+<script>write_email( "sigcse12-src", "cs.holycross.edu" );</script><br/>
+<img src="/sigcse2012/images/photos/ann_sobel.jpg" /></p>
 </td></tr>
 </table>
 
