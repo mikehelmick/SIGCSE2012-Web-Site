cefd4ef860c644bd19113110cd5248b31923de98
Mike Helmick <mike.helmick@gmail.com>
2011-05-10 21:25:59 -0700

    Add APC photos. Closes #15.

--- committee/apc.php
+++ committee/apc.php
@@ -13,15 +13,15 @@
 <p><strong>Associate Program Chairs</strong></p>
   </td><td>&nbsp;</td></tr>
 
-<tr><td valign="top" align="left"><p>Doug Baldwin<br/>SUNY Geneseo<br/><script>write_email("baldwin", "geneseo.edu");</script></p></td><td valign="top" align="left">&nbsp;</td></tr>
-<tr><td valign="top" align="left"><p>Joel Adams<br/>Calvin College<br/><script>write_email("adams", "calvin.edu");</script></p></td><td valign="top" align="left">&nbsp;</td></tr>
-<tr><td valign="top" align="left"><p>Tim Fossum<br/>SUNY Potsdam<br/><script>write_email("fossumtv", "potsdam.edu");</script></p></td><td valign="top" align="left">&nbsp;</td></tr>
-<tr><td valign="top" align="left"><p>Nancy Kinnersley<br/>University of Kansas<br/><script>write_email("kinners", "eecs.ku.edu");</script></p></td><td valign="top" align="left">&nbsp;</td></tr>
-<tr><td valign="top" align="left"><p>Sheila Castaneda<br/>Clarke College<br/><script>write_email("sheila.castaneda", "clarke.edu");</script></p></td><td valign="top" align="left">&nbsp;</td></tr>
-<tr><td valign="top" align="left"><p>Lori Pollock<br/>University of Delaware<br/><script>write_email("pollock", "cis.udel.edu");</script></p></td><td valign="top" align="left">&nbsp;</td></tr>
-<tr><td valign="top" align="left"><p>Tim Bell<br/>University of Canterbury<br/><script>write_email("tim.bell", "canterbury.ac.nz");</script></p></td><td valign="top" align="left">&nbsp;</td></tr>
-<tr><td valign="top" align="left"><p>Sami Rollins<br/>University of San Francisco<br/><script>write_email("srollins", "cs.usfca.edu");</script></p></td><td valign="top" align="left">&nbsp;</td></tr>
-<tr><td valign="top" align="left"><p>Sherri Goings<br/>Carleton College<br/><script>write_email("sgoings", "carleton.edu");</script></p></td><td valign="top" align="left">&nbsp;</td></tr>
+<tr><td valign="top" align="left"><p class="listing">Doug Baldwin<br/>SUNY Geneseo<br/><script>write_email("baldwin", "geneseo.edu");</script></p></td><td valign="top" align="left"><img src="/sigcse2012/images/photos/apc/Baldwin.png" /></td></tr>
+<tr><td valign="top" align="left"><p class="listing">Joel Adams<br/>Calvin College<br/><script>write_email("adams", "calvin.edu");</script></p></td><td valign="top" align="left"><img src="/sigcse2012/images/photos/apc/Adams.jpg" /></td></tr>
+<tr><td valign="top" align="left"><p class="listing">Tim Fossum<br/>SUNY Potsdam<br/><script>write_email("fossumtv", "potsdam.edu");</script></p></td><td valign="top" align="left"><img src="/sigcse2012/images/photos/apc/Fossum.png" /></td></tr>
+<tr><td valign="top" align="left"><p class="listing">Nancy Kinnersley<br/>University of Kansas<br/><script>write_email("kinners", "eecs.ku.edu");</script></p></td><td valign="top" align="left"><img src="/sigcse2012/images/photos/apc/Kinnersley.jpg" /></td></tr>
+<tr><td valign="top" align="left"><p class="listing">Sheila Castaneda<br/>Clarke College<br/><script>write_email("sheila.castaneda", "clarke.edu");</script></p></td><td valign="top" align="left"><img src="/sigcse2012/images/photos/apc/Castaneda.jpg" /></td></tr>
+<tr><td valign="top" align="left"><p class="listing">Lori Pollock<br/>University of Delaware<br/><script>write_email("pollock", "cis.udel.edu");</script></p></td><td valign="top" align="left"><img src="/sigcse2012/images/photos/apc/Polluc.jpg" /></td></tr>
+<tr><td valign="top" align="left"><p class="listing">Tim Bell<br/>University of Canterbury<br/><script>write_email("tim.bell", "canterbury.ac.nz");</script></p></td><td valign="top" align="left"><img src="/sigcse2012/images/photos/apc/Bell.jpg" /></td></tr>
+<tr><td valign="top" align="left"><p class="listing">Sami Rollins<br/>University of San Francisco<br/><script>write_email("srollins", "cs.usfca.edu");</script></p></td><td valign="top" align="left"><img src="/sigcse2012/images/photos/apc/Rollins.jpg" /></td></tr>
+<tr><td valign="top" align="left"><p class="listing">Sherri Goings<br/>Carleton College<br/><script>write_email("sgoings", "carleton.edu");</script></p></td><td valign="top" align="left"><img src="/sigcse2012/images/photos/apc/Goings.jpg" /></td></tr>
 
 </table>
 <?php
