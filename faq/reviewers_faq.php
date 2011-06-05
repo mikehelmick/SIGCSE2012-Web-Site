ed9bf4c90a175dc5005a822752fe8b4f55f10be8
Mike Helmick <mike.helmick@gmail.com>
2011-06-05 13:29:49 -0700

    update reviewer's faq. Closes #30.

--- faq/reviewers_faq.php
+++ faq/reviewers_faq.php
@@ -9,7 +9,7 @@
 </div></div>
 <ol>
   <li> <em>What should I do if I'm assigned a paper that I am not
-qualified to review?</em><p>Please contact the Program Chair(s) as soon as possible inform them of the problem.  The Program Chair(s) assign you an alternate paper.</p></li>
+qualified to review?</em><p>Please contact the Program Chair(s) as soon as possible and inform them of the problem. The Program Chair(s) will likely assign you an alternate paper.</p></li>
   <li> <em>What should I do if I recognize the authors of a paper?</em><p>Make a value judgment.  If you feel you can impartially review the paper then go ahead and review it.  If you feel uncomfortable reviewing the paper then contact the Program Chair(s) as soon as possible.</p></li>
   <li><em>What should I do if I notice plagiarism in a paper?</em><p> SIGCSE will not accept papers that contain plagiarized material.  Please contact the Program Chair(s) immediately.</p></li>
   <li><em>Why is Photoshop trying to open my papers, and what can I do about it?</em><p style="font-size: 1.0em">For security reasons, reviewers receive papers via a URL that uses a <tt>.asp</tt> file extension. This extension is used for Active Server Pages, but also for Photoshop photo indices.  If you have Photoshop installed, your web browser or operating system may believe that Photoshop is the appropriate application to access the file's content. Here are two suggestions to deal with this problem:</p>
