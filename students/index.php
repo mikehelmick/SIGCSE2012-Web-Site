bf9c5762f31322626c200d9e2c4c47df8596639a
Mike Helmick <mike.helmick@gmail.com>
2011-08-16 19:39:41 -0700

    add general student information and student volunteer page.

--- students/index.php
+++ students/index.php
@@ -1,13 +1,40 @@
 <?php
  $title = "SIGCSE 2012 - Students";
- $onload = "pageLoad('students', 'studentsGeneral');";
+ $onload = "pageLoad('students', 'students_general');";
  include("../parts/top.php");
 ?>
 <div class="SectionHeader"><div class="Full">
-<h1>SIGCSE 2012 - Information for Students</h1>
+<h1>Students @ SIGCSE 2012</h1>
 </div></div>
 
-<p>Coming soon...</p>
+<h2>How can students get involved?</h2>
+<p>We're glad you asked - students can get involved in several ways:
+<ul>
+  <li><a href="/sigcse2012/authors/student_research.php">Student Research Competition</a></li>
+  <li>Of course, students can also become <a href="/sigcse2011/authors/">authors</a> or regular <a href="/sigcse2012/attendees/">conference attendees</a> (at a reduced registration rate).</li>
+  <li><a href="/sigcse2011/students/volunteer.php">Volunteer at SIGCSE</a></li>
+</ul>
+
+<p>Students at SIGCSE 2008</p>
+<table border="0" align="center">
+<tr>
+  <td><a href="images/IMG_5695.JPG" rel="lightbox[students]" title="Students having FUN at SIGCSE 2008"><img src="images/IMG_5695_thumb.jpg" /></a></td>
+  <td><a href="images/IMG_5767.JPG" rel="lightbox[students]" title="Students having FUN at SIGCSE 2008"><img src="images/IMG_5767_thumb.jpg" /></a></td>
+  <td><a href="images/IMG_5782.JPG" rel="lightbox[students]" title="Students having FUN at SIGCSE 2008"><img src="images/IMG_5782_thumb.jpg" /></a></td>
+  <td><a href="images/IMG_5864.JPG" rel="lightbox[students]" title="Students having FUN at SIGCSE 2008"><img src="images/IMG_5864_thumb.jpg" /></a></td>
+</tr>
+</table>
+
+<p>Students at SIGCSE 2007</p>
+
+<table border="0" align="center">
+<tr>
+  <td><a href="images/sigcse020.jpg" rel="lightbox[students]" title="Students having FUN at SIGCSE 2007"><img src="images/sigcse020_thumb.jpg" /></a></td>
+  <td><a href="images/sigcse027.jpg" rel="lightbox[students]" title="Students having FUN at SIGCSE 2007"><img src="images/sigcse027_thumb.jpg" /></a></td>
+  <td><a href="images/sigcse033.jpg" rel="lightbox[students]" title="Students having FUN at SIGCSE 2007"><img src="images/sigcse033_thumb.jpg" /></a></td>
+</tr>
+</table>
+
 
 <?php
  $updated = filemtime( $_SERVER["SCRIPT_FILENAME"] );
---
+++ students/volunteer.php
@@ -0,0 +1,98 @@
+<?php
+ $title = "SIGCSE 2012 - Students";
+ $onload = "pageLoad('students', 'students_general');";
+ include("../parts/top.php");
+?>
+<div class="SectionHeader"><div class="Full">
+<h1>Student Volunteers @ SIGCSE 2012</h1>
+</div></div>
+<p>Student volunteers are an important part of the SIGCSE Symposium and have been for many years.  Each volunteer works for up to 6 hours, helping with various conference management activities.  You'll interact with the conference organizers and presenters and will meet other students from all over the world.  More information about the experience, the benefits, and how to sign up are provided below.</p>
+<!-- <p>Student Volunteer registration is now closed.  You may still attend the conference; register to attend on the SIGCSE 2012 registration page).  Any questions should be directed to Briana Morrison at <script>write_email( "sigcse11-volunteers", "cs.cmu.edu" );</script></p> -->
+<p>To volunteer you should register to attend the SIGCSE symposium (the registration options are detailed on our <a href="/sigcse2012/attendees/registration.php">SIGCSE 2012 registration page</a>) AND you should <a href="http://www.surveymonkey.com/s/688FTXR">sign up to be a student volunteer</a>.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2>What do I have to do to become a student volunteer?</h2>
+</div></div>  
+<p>Each student volunteer is asked to sign up as a volunteer prior to the conference.  (Instructions for signing up as a student volunteer are given below.) Upon arrival at the conference site, you must check in at the Registration Desk to receive your conference registration packet then visit the Student Volunteer Office to pick up your work schedule (with a list of times, locations, and duties).</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2>Who is eligible to be a student volunteer?</h2>
+</div></div> 
+<p>Any full time student may become a student volunteer. Both undergraduate and graduate students are welcome. You specify your school affiliation when you sign up as a volunteer, and you should bring a copy of your current course schedule to the conference as verification of your full time status.  Students attending the conference for the Research Poster competition are encouraged to become volunteers as well.</p>
+
+<p><strong class="boldnote">Important information for students requiring a visa:</strong> The Association for Computing Machinery (ACM, SIGCSE's umbrella organization) does not issue formal invitation letters for visas.  However, they can issue a visa support letter.  For a visa support letter, please send a request to <a href="mailto:Bucci@hq.acm.org">Laura Bucci (Bucci@hq.acm.org)</a>with the following information:</p>
+<p><ol>
+ <li>Name and mailing address as it appears on your passport</li>
+ <li>The name of the conference you wish to attend (SIGCSE 2012)</li>
+ <li>Registration confirmation number</li>
+ <li>If you are the author of any papers accepted for the conference, please provide the title</li>
+ <li>Fax number (visa support letters are not issued via email)</li>
+</ol></p>
+
+<div class="SectionHeader"><div class="Full">
+<h2>What do I get in return for being a student volunteer?</h2>
+</div></div> 
+<p>In exchange for 6 hours of conference service, you will receive:</p>
+<p><ul>
+ <li>Refunded registration fee.</li>
+ <li>Entrance to the student volunteer party.</li>
+ <li>A chance to meet and work with students from other schools.</li>
+ <li>The opportunity to contribute to an effort to improve Computer Science education ... and our sincere gratitude for your efforts.</li>
+</ul></p>
+
+<p>Note that in addition to these benefits, since you are a registered SIGCSE student attendee, you have all the rights of the non-volunteer student attendees:</p>
+<p><ul>
+ <li>Access to all sessions, including papers, panels, seminars, and birds-of-a-feather</li>
+ <li>Access to all exhibits where book publishers and software vendors have displays and activities</li>
+ <li>All conference &quot;coffee&quot; breaks (food and drink)</li>
+ <li>The main conference reception on Thursday evening</li>
+ <li>The conference luncheon on Saturday afternoon</li>
+</ul></p>
+
+<div class="SectionHeader"><div class="Full">
+<h2>What kinds of work will I do as a student volunteer?</h2>
+</div></div> 
+<p>
+It varies. You might work at the registration booth to help organize the packets that we give out to attendees. You might be at the door of the exhibits hall to verify that the people entering have a conference badge. You might count the participants at various sessions. You might hang out at certain sessions to assist the speakers with handouts or equipment.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2>Do I have a say in what work I do as a student volunteer?</h2>
+</div></div> 
+<p> Not much, but we'll do what we can.  There are often fewer volunteers than we have jobs, so we have to prioritize.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2>When during the conference will I do my student volunteer work?</h2>
+</div></div> 
+<p>This issue is driven by conference needs. Some time periods are busier than others. We'll take your preferences into account whenever possible. When you sign up as a student volunteer, you'll specify when you plan to arrive and leave the conference. Within those constraints, we'll make some initial plans. We realize that things may come up and will try to make dynamic changes as needed. In return, we ask that you be as flexible as possible. Your duties may be broken into separate time periods and even across separate days.  We guarantee no more than 6 hours of assigned work per volunteer but target an average of 5 hours.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2>Do I have to be there for the whole conference to be a student volunteer?</h2>
+</div></div> 
+<p>No. We realize that travel plans and other commitments exist. We hope that students local to the Dallas area will be flexible.  In particular, there is always a need for volunteers on Tuesday and Wednesday, which is before most of the conference activities begin and before many of the traveling student volunteers have arrived. We encourage local student volunteers to be available on Tuesday and Wednesday.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2>Can I get funding for travel or hotel expenses?</h2>
+</div></div> 
+<p>The conference itself does not provide funding for student volunteers, but you may be able to get some support through your department or through another source at your university.  You should print your registration acknowledgment to show to potential funding sources.</p>
+
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="questions">Questions</a></h2>
+</div></div>   
+
+<table>
+<tr><td colspan="2"><b>Student Volunteers and Student Activities</b></td></tr>
+<tr><td>
+Mary Anne Egan<br/>
+Siena College<br/>
+</td><td>
+Steven Huss-Lederman</p>
+Beloit College<br/>
+</td></tr>
+<tr><td colspan="2"><script>write_email( "sigcse12-volunteers", "cs.holycross.edu" );</script></td></tr></table>
+
+<?php
+ $updated = filemtime( $_SERVER["SCRIPT_FILENAME"] );
+ $mainPage = true;
+ include("../parts/bottom.php");
+?>
