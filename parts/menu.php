521834d20b8839f355c409e263daea53ba485d65
Mike Helmick <mike.helmick@gmail.com>
2011-10-19 22:14:53 -0700

    add blog link to menu. Closes #85.

--- parts/menu.php
+++ parts/menu.php
@@ -4,6 +4,9 @@
 ?>
 
 <div class="menu"><a id="home_menu" href="/sigcse2012/">SIGCSE | 2012</a></div>
+<div class="menu">
+ <a href="http://sigcse2012.blogspot.com">Blog</a> |
+ <a href="http://twitter.com/#!/sigcse2012">Twitter</a></div>
 <div class="menu"><a id="authors_menu" onclick="toggle_menu('authors');">Authors<img id="authors_status" src="/sigcse2012/images/arrow_down.png" align="absmiddle"></a></div>	
 <div id="authors_sub" class="submenu" style="<?php echo $menuStyle; ?>">
   <ul>
