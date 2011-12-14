22cfeec60e7e7cbdfdaac34a067ae3f619692f6f
Mike Helmick <mike.helmick@gmail.com>
2011-12-13 22:22:57 -0800

    Add kids camp details. Closes #101.

--- attendees/kids_camp.php
+++ attendees/kids_camp.php
@@ -8,10 +8,65 @@
 <h1>SIGCSE 2012 - Kids Camp</h1>
 </div></div>
 
-<div class="Alert">Details for the SIGCSE 2012 Kids Camp are not yet available, please check back soon.</div>
+<p>We are pleased to announce that we will once again have Kids’ Camp at SIGCSE 2012!</p>
 
-<p><img src="/sigcse2011/images/logos/sponsors/microsoft.png" align="left"  class="headshot"/> Computers and t-shirts for Kids Camp are available due to the generous support of Microsoft. Thank you!
-&nbsp;<br/>&nbsp;<br/>&nbsp;</p>
+<p><img src="/sigcse2011/images/logos/sponsors/microsoft.png" align="left"  class="headshot"/>A big ‘Thank You’ to Microsoft who is generously providing T-shirts for enrolled participants and equipment for the various planned activities.<br/>&nbsp;<br/>&nbsp;</p>
+
+<p>The SIGCSE 2012 Kids’ Camp will be located in Room 402 on the Ballroom Level (4th floor) of the Raleigh Convention Center.</p>
+
+<p>Registration will soon be available through KiddieCorp’s website. To learn more about KiddieCorp, our children's program provider, please visit <a href="https://www.kiddiecorp.com">https://www.kiddiecorp.com</a>.</p>
+
+<table width="90%" style="padding-left: 25px;">
+ <tr><td width="50%" align="left" valign="top">
+<p><strong>Kids’ Camp Dates and Times:</strong>
+<ul><li>Thursday, March 1<ul>
+ <li>8:00am - 1:00pm</li>
+ <li>1:00pm - 6:00pm</li></ul></li>
+<li>Friday, March 2<ul>
+ <li>8:00am - 1:00pm</li>
+ <li>1:00pm - 6:00pm</li></ul></li>
+<li>Saturday, March 3<ul>
+ <li>8:00am - 2:30pm</li></ul></li>
+</ul></p>
+ </td><td width="50%" align="left" valign="top">
+<p><strong><a name="#fees">Fees:</a></strong>
+<ul><li>Children 6 months to 2 years:<ul>
+ <li>Half day: $45 per child</li>
+ <li>Full day: $76 per child</li></ul></li>
+<li>Children 3 to 12 years:<ul>
+ <li>Half day: $40 per child</li>
+ <li>Full day: $76 per child</li>
+</ul></li></ul></p>
+ </td></tr>
+</table>
+
+<div class="SectionHeader"><div class="Full">
+<h1>Frequently Asked Questions</h1>
+</div></div>
+
+<ol>
+ <li><p><strong>How old/young can my child(ren) be to register?</strong><br/>
+Care will be provided for children ages 6 months to 12 yrs. If you have an older child who wants to attend, please contact us.</p></li>
+ <li><p><strong>How much does it cost?</strong><br/>
+See the <a href="#fees">fees</a> section above.</p></li>
+ <li><p><strong>Where will Kids Camp be located?</strong><br/>
+Kids Camp is conveniently located in Room 402 on the Ballroom Level (4th floor) of the Raleigh Convention Center, close to conference activities. Visiting your children will be easy, yet they are comfortably away from the main conference traffic. Restrooms are located nearby. You can see the room location on the <a href="/sigcse2012/attendees/floor_plans.php">SIGCSE 2012 floor plans</a>.</p></li>
+ <li><p><strong>What will my child(ren) be doing while I am attending the conference?</strong><br/>
+SIGCSE Kid Campers will participate in drama, play, and arts & crafts activities. Older kids can participate in our planned computing activities (details will be provided soon). Student volunteers will help with the computing activities.</p></li>
+ <li><p><strong>What will my children be served for lunch?</strong><br/>
+Lunch menu selections are still being planned. Last year's menu included entrees such as chicken fingers, grilled cheese, macaroni & cheese, and pizza (with 1 topping). Side dishes were celery & carrot sticks, fruit cup, and a selection of whole fruit (apple, banana, orange).</p></li>
+ <li><p><strong>Do I need to bring anything for my child(ren)?</strong><br/>
+Yes. A change of clothes is strongly recommended for all children. In addition, parents of young children will need to provide any diapers, wipes, baby food, blankets, formula and bottles that may be needed.</p></li>
+ <li><p><strong>How many childcare providers will be in the rooms?</strong><br/>
+Daycare provider ratios will be:
+<ul>
+ <li>1:2 for children ages 6 to 11 months old</li>
+ <li>1:3 for children 1 to 2 years old</li>
+ <li>1:5 for children 3 to 5 years old</li> 
+ <li>1:7 for children 6 to 12 years old</li>
+</ul>
+Student volunteers for the computing activities will be additional helpers and are not included in this ratio.</p></li>
+</ol>
 
 <?php
  $updated = filemtime( $_SERVER["SCRIPT_FILENAME"] );
