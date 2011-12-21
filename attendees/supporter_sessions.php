41dc75d67fd89c0428fe237d11f71df07ad79f6b
Mike Helmick <mike.helmick@gmail.com>
2011-12-20 22:12:06 -0800

    add microsoft supporter sessions.

--- attendees/supporter_sessions.php
+++ attendees/supporter_sessions.php
@@ -10,10 +10,13 @@
 
 <ul>
  <li><strong>Thursday, March 1, 2012</strong><ul>
-  <li><a href="#google">3:45PM – 5:00PM - Google</a></li>
+  <li><a href="#microsoft">10:45 am -12:00 pm - Microsoft</a></li>
+  <li><a href="#microsoft">1:45 pm - 3:00 pm - Microsoft</a></li>
+  <li><a href="#google">3:45 pm – 5:00 pm - Google</a></li>
  </ul></li>
  <li><strong>Friday, March 2, 2012</strong><ul>
-  <li><a href="#google">1:45PM – 3:00PM - Google</a></li>
+  <li><a href="#google">1:45 pm – 3:00 pm - Google</a></li>
+  <li><a href="#microsoft">3:45 pm -5:00 pm - Google</a></li>
  </ul></li>
 </ul>
 
@@ -41,6 +44,40 @@ Mark Friedman, Former App Inventor Project Lead Google</p>
 <li>TaleBlazer, from the Center’s Scheller Teacher Education Program, is a location-based augmented reality mobile gaming platform, where people use blocks programming to create behaviors for characters in the game. </li>
 <li>App Inventor is a programming tool that makes it easy, even for beginners, to create applications for Android phones.  App Inventor was incubated at Google Research, and has been transferred to the Center, which is continuing its development as an open source system and providing it as an open service for educational use.</li>
 </ol></p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="microsoft">Microsoft</a></h2>
+</div></div>
+
+<p><strong>Empowering Students: Teaching Software Development with Windows Phone</strong><br/>
+Thursday March 1, 10:45 am - 12:00 pm<br/>
+Rob Miles, University of Hull</p>
+<p>With Windows Phone it is really easy to make publishing applications and games part of the learning experience. Students love being able to share their work with friends, family and even future employers. In this session you’ll discover the wealth of Windows Phone based teaching resources available and how they can be used to give students a head start in creating useful applications (including use of Cloud) and entertaining gameplay for the Windows Phone platform, while they are at the same time learning software development techniques.</p>
+
+<p><strong>Creative uses for Kinect in Teaching – with curriculum materials</strong><br/>
+Thursday 1:45 pm - 3:00 pm<br/>
+Rob Miles, university of Hull</p>
+<p>The Kinect sensor is the "Fastest Selling Consumer Electronics Gadget in History". It is a great way to add a new dimension to Xbox 360 gameplay, able to read its environment and track the body movement of players. It is also a great teaching tool and a genuinely creative device. In this session Rob Miles will show how you can harness this creativity and get students enjoying themselves while writing programs that make use of the unique abilities of this amazing sensor and its accompanying Kinect for Windows software. He will also have curriculum materials to share with you that you can use freely in your own classes. </p>
+<p><em>speaker bio:</em> Rob Miles has been writing programs and teaching how to create them since computers were programmed using punched cards and output came from line printers. Nowadays he much prefers Visual Studio and C#, but over the years he has worked on a huge range of languages and platforms, frequently for money, having worked on projects as diverse as putting dates tamps on Budweiser beer and managing toll collection for the Humber Bridge. He teaches C# programming and Software Development at the University of Hull in the UK; where over the years hundreds of students have suffered through his jokes and picked up some development skills along the way. Rob is a Windows Phone MVP and is also Judges Captain for the Imagine Cup Software Development competition. He has written books on XNA development and the .NET Micro Framework and is presently working on a book to introduce programmers to the joys of the Kinect sensor and the fun you can have with it. He also claims to know lots of jokes, some of them about cheese.</p>
+
+<p><strong>Cloud in a Classroom:  Faculty Experiences</strong><br/>
+Friday March 2, 3:45 pm - 5:00 pm<br/>
+<ul>
+<li>Nilanjan Banerjee, University of Arkansas, US</li>
+<li>Chia-Chi Teng, Brigham Young University, US</li>
+<li>Alexander Schmidt, University of Potsdam, Germany</li>
+<li>Moderator: Arkady Retik, Microsoft, Redmond</li>
+</ul>
+
+<p>Cloud computing introduces new and exciting opportunities for computing industry.  To realize the potential of cloud computing in higher education, one must think about the cloud as a holistic platform for creating new services, new experiences, and new methods for research and teaching.  Pursuing these goals in the current set of the CS courses presents a broad range of interesting questions.  Come along to hear about the cloud-based teaching resources available and how they have been used in universities world-wide.</p>
+<p>This panel will provide an opportunity for SIGCSE attendees to hear from faculty who have been teaching CS courses using Windows Azure ask questions and discuss and share their own experiences.</p>
+<p><em>speaker bios:</em>
+<ul>
+<li>Nilanjan Banerjee is an Assistant Professor at University of Arkansas, Fayetteville.   His research interests are in the areas of mobile systems, healthcare systems, and renewable energy driven systems. He has been associated with Project Hawaii (aka Mobile Azure) for one and a half years.</li>
+<li>Chia-Chi Teng had over 16 years of R&D experiences in the software industry before joining the faculty at Brigham Young University. He enjoys mentoring undergrad students in research or capstone projects.</li>
+<li>Alexander Schmidt worked in the Operating Systems and Middleware group at Hasso Plattner Institute in Potsdam, Germany, where he contributed for more than 5 years to the Windows Research Kernel project. Since 2009, Alexander has been associated with the InstantLab project to leverage Cloud computing capabilities for teaching operating systems.</li>
+</ul>
+</p>
 
 
 <?php
